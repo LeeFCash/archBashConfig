f82a336d03c1b6a7610cd700467319bdd8f119c1
LeeFCash <leecash133@gmail.com>
2026-04-02 01:36:03 -0400

    made it less lines and added some other stuff

--- archConfig.sh
+++ archConfig.sh
@@ -8,6 +8,10 @@
 
 set -e # stops the script when error
 
+R=$((RANDOM % 2))
+echo "${R}" > /tmp/random.txt
+R=$(cat /tmp/random.txt)
+
 update=false
 installFirefox=true
 installBrave=true
@@ -31,6 +35,12 @@ installBlueman=true
 installBlender=true
 installTmux=true
 installNeovim=true
+installGamescope=true # you may need nvidia-utils(GPU stuff)
+installFastfetch=true
+installFlatpak=true
+installMesa=true
+installTtffontawesome=true
+installProtonGeCustomBin=true
 
 groupInstallNetworkAndStart=true
 
@@ -103,19 +113,27 @@ is_installed() {
 manage_install_pkg() {
 	local flag1="$1"
 	local pkg="$2"
+	local setUp="$3"
 
 	if [[ "$flag1" == true ]] && ! is_installed "$pkg"; then
 		echo "installing $pkg"
 		sudo pacman -S "$pkg"
+		if [[ "$pkg" == "git" ]] && [[ "$setUp" == true ]]; then
+			echo "setting up git LeeFCash"
+			git config --global user.email "leecash133@gmail.com"
+			git config --global user.name "LeeFCash"
+			echo "git is ready LeeFCash"
+		fi
+		if [[ "$pkg" == "virt-manager" ]] && [[ "$setUp" == true ]]; then
+			echo " Add user to libvirt group "
+			sudo usermod -aG libvirt "$USER"
+			echo "Setting up libvirt default network..."
+			sudo virsh net-start default
+			sudo virsh net-autostart default
+		fi
 	elif [[ "$flag1" == false ]] && is_installed "$pkg"; then
 		echo "uninstalling $pkg"
 		sudo pacman -Rns "$pkg"
-	fi
-	
-	if [[ "$pkg" == "virt-manager" ]] && [[ "$flag1" == true ]] && [[ "$virshStartDefaultAndAuto" == true ]]; then
-		echo "Setting up libvirt default network..."
-		sudo virsh net-start default
-		sudo virsh net-autostart default
 	fi
 }
 
@@ -141,6 +159,13 @@ manage_install_pkg_with_yay() {
 		sudo pacman -Rns "$pkg"
 	fi
 }
+
+if $update; then
+	echo "update the system..."
+	sudo pacman -Syu
+	yay
+fi
+
 # without yay
 manage_install_pkg $installOBSstudio obs-studio
 manage_install_pkg $installDolphin dolphin
@@ -154,10 +179,41 @@ manage_install_pkg $installNetworkManager networkmanager
 manage_install_pkg $installBlender blender
 manage_install_pkg $installTmux tmux
 manage_install_pkg $installNeovim neovim
+manage_install_pkg $installGamescope gamescope
+manage_install_pkg $installFastfetch fastfetch
+manage_install_pkg $installFlatpak flatpak
+manage_install_pkg $installMesa mesa
+manage_install_pkg $installTtffontawesome ttf-font-awesome
+manage_install_pkg $installFirefox firefox
+manage_install_pkg $installBadeDevel base-devel
+manage_install_pkg $installGit git true
+manage_install_pkg $installSteam steam
+manage_install_pkg $installVulkanIcdLoader vulkan-icd-loader
+manage_install_pkg $installLib32VulkanIcdLoader lib32-vulkan-icd-loader
+manage_install_pkg $installQemu qemu
+manage_install_pkg $installVde2 vde2
+manage_install_pkg $installIptables iptables
+manage_install_pkg $installDnsmasq dnsmasq
+manage_install_pkg $installIproute2 iproute2
+manage_install_pkg $installOpenbsdNetcat openbsd-netcat
+manage_install_pkg $installSwtpm swtpm
+manage_install_pkg $installVirtManager virt-manager true
+manage_install_pkg $installWayland wayland
+manage_install_pkg $installNiri niri
+manage_install_pkg $installXorgXwayland xorg-xwayland
+manage_install_pkg $installWlClipBoard wl-clipboard
+manage_install_pkg $installFoot foot
+manage_install_pkg $installFuzzel fuzzel
+manage_install_pkg $installGrim grim
+manage_install_pkg $installSlurp slurp
+manage_install_pkg $installSwaybg swaybg
 # with yay
 manage_install_pkg_with_yay $installTeamsForLinuxBin teams-for-linux-bin
 manage_install_pkg_with_yay $installDiscordptb discord-ptb
-
+manage_install_pkg_with_yay $installXwaylandSatellite xwayland-satellite
+manage_install_pkg_with_yay $installBrave brave-bin
+manage_install_pkg_with_yay $installProtonGeCustomBin proton-ge-custom-bin
+# -----
 if [ ! -d "/home/leecash/AppImages" ]; then
 	mkdir /home/leecash/AppImages
 fi
@@ -167,72 +223,8 @@ if $groupInstallNetworkAndStart && is_installed networkmanager && ! systemctl is
 	sudo systemctl enable --now NetworkManager
 fi
 
-if $update; then
-	echo "update the system..."
-	sudo pacman -Syu
-fi
-
-if $installFirefox && ! is_installed firefox; then
-	echo "installing firefox"
-	sudo pacman -S firefox
-elif ! $installFirefox && is_installed firefox; then
-	echo "uninstalling firefox"
-	sudo pacman -Rns firefox
-fi
-
-if $installBaseDevel && ! is_installed base-devel; then
-	echo "installing base-devel..."
-	sudo pacman -S base-devel # dependencies for brave
-elif ! $installBaseDevel && is_installed base-devel; then
-	echo "uninstalling base-devel..."
-	sudo pacman -Rns base-devel
-fi
-
-if $installGit && ! is_installed git; then
-	echo "installing and seting up git..."
-	sudo pacman -S git
-	git config --global user.email "leecash133@gmail.com"
-	git config --global user.name "LeeFCash"
-	echo "git is ready LeeFCash"
-elif ! $installGit && is_installed git; then
-	echo "uninstalling git"
-	sudo pacman -Rns git
-fi
-
 if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
 	echo "WARNING: multilib repo may not be enabled!"
-fi
-
-if $installSteam && ! is_installed steam; then
-	echo "installing steam..."
-	sudo pacman -S steam
-elif ! $installSteam && is_installed steam; then
-	echo "uninstalling steam..."
-	sudo pacman -Rns steam
-fi
-
-
-if $installVulkanIcdLoader && ! is_installed vulkan-icd-loader; then
-		sudo pacman -S vulkan-icd-loader
-elif ! $installVulkanIcdLoader && is_installed vulkan-icd-loader; then
-		sudo pacman -Rns vulkan-icd-loader
-fi
-
-if $installLib32VulkanIcdLoader && ! is_installed lib32-vulkan-icd-loader; then
-		sudo pacman -S lib32-vulkan-icd-loader
-elif ! $installLib32VulkanIcdLoader && is_installed lib32-vulkan-icd-loader; then
-		sudo pacman -Rns lib32-vulkan-icd-loader
-fi
-
-if $installVirtManager && ! is_installed virt-manager; then
-	echo "Installing Virt-Manager and virtualization stack..."
-	# Core virtualization packages
-	sudo pacman -S virt-manager
-
-	# Add user to libvirt group
-	sudo usermod -aG libvirt "$USER"
-elif ! $installVirtManager && is_installed virt-manager; then
-	sudo pacman -Rns virt-manager
 fi
 
 if $installVirtManager && ! systemctl is-active --quiet libvirtd; then
@@ -242,128 +234,8 @@ if $installVirtManager && ! systemctl is-active --quiet libvirtd; then
 	#sudo systemctl enable --now libvirtd.service virtlogd.service
 fi
 
-if $installQemu && ! is_installed qemu; then
-	sudo pacman -S qemu
-elif ! $installQemu && is_installed qemu; then
-	sudo pacman -Rns qemu
-fi
-
-if $installVde2 && ! is_installed vde2; then
-	sudo pacman -S vde2
-elif ! $installVde2 && is_installed vde2; then
-	sudo pacman -Rns vde2
-fi
-
-if $installIptables && ! is_installed iptables; then
-	sudo pacman -S iptables
-elif ! $installIptables && is_installed iptables; then
-	sudo pacman -Rns iptables
-fi
-
-if $installDnsmasq && ! is_installed dnsmasq; then
-	sudo pacman -S dnsmasq
-elif ! $installDnsmasq && is_installed dnsmasq; then
-	sudo pacman -Rns dnsmasq
-fi
-
-if $installIproute2 && ! is_installed iproute2; then
-	sudo pacman -S iproute2
-elif ! $installIproute2 &&  is_installed iproute2; then
-	sudo pacman -Rns iproute2
-fi
-
-if $installOpenbsdNetcat && ! is_installed openbsd-netcat; then
-	sudo pacman -S openbsd-netcat
-elif ! $installOpenbsdNetcat && is_installed openbsd-netcat; then
-	sudo pacman -Rns openbsd-netcat
-fi
-
-if $installPolkitGnome && ! is_installed polkit-gnome; then
-	echo "just here for later"
-	#sudo pacman -S polkit-gnome # I don't need
-elif ! $installPolkitGnome && is_installed polkit-gnome; then
-	echo "just here for later2"
-	#sudo pacman -Rns polkit-gnome # I don't need 
-fi
-
-if $installSwtpm && ! is_installed swtpm; then
-	sudo pacman -S swtpm
-elif ! $installSwtpm && is_installed swtpm; then
-	sudo pacman -Rns swtpm
-fi
-
-if $installXwaylandSatellite && $installYay && ! is_installed xwayland-satellite; then
-	yay -S xwayland-satellite
-elif ! $installXwaylandSatellite &&  is_installed xwayland-satellite; then
-	sudo pacman -Rns xwayland-satellite
-fi
-
-if $installBrave && $installYay && ! is_installed brave-bin; then
-	echo "installing brave browser..."
-	yay -S brave-bin
-elif ! $installBrave && is_installed brave-bin; then
-	echo "uninstalling brave browser..."
-	sudo pacman -Rns brave-bin
-fi
-
-if $installNiri && $installYay && ! is_installed niri; then
-	yay -S niri
-elif ! $installNiri && is_installed niri; then
-	sudo pacman -Rns niri
-fi
-
-if $installWayland && ! is_installed wayland; then
-	sudo pacman -S wayland
-elif ! $installWayland && is_installed wayland; then
-	sudo pacman -Rns wayland
-fi
-
-if $installXorgXwayland && ! is_installed xorg-xwayland; then
-	sudo pacman -S xorg-xwayland
-elif ! $installXorgXwayland && is_installed xorg-xwayland; then
-	sudo pacman -Rns xorg-xwayland
-fi
-
-if $installWlClipBoard && ! is_installed wl-clipboard; then
-	sudo pacman -S wl-clipboard
-elif ! $installWlClipBoard && is_installed wl-clipboard; then
-	sudo pacman -Rns wl-clipboard
-fi
-
-if $installFoot && ! is_installed foot; then
-	sudo pacman -S foot
-elif ! $installFoot && is_installed foot; then
-	sudo pacman -Rns foot
-fi
-
-if $installFuzzel && ! is_installed fuzzel; then
-	sudo pacman -S fuzzel
-elif ! $installFuzzel && is_installed fuzzel; then
-	sudo pacman -Rns fuzzel
-fi
-
-if $installGrim && ! is_installed grim; then
-	sudo pacman -S grim
-elif ! $installGrim && is_installed grim; then
-	sudo pacman -Rns grim
-fi
-
-if $installSlurp && ! is_installed slurp; then
-	sudo pacman -S slurp
-elif ! $installSlurp && is_installed slurp; then
-	sudo pacman -Rns slurp
-fi
-
-if $installSwaybg && ! is_installed swaybg; then
-	sudo pacman -S swaybg
-elif ! $installSwaybg && is_installed swaybg; then
-	sudo pacman -Rns swaybg
-fi
-
-if $installMako && ! is_installed mako; then
-	sudo pacman -S mako
-elif ! $installMako && is_installed mako; then
-	sudo pacman -Rns mako
+if [[ "$R" == "1" ]]; then
+	fastfetch
 fi
 
 echo "script is done"
